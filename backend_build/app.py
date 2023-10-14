9b6c7987227110db014dcaf7ff593acd9fe52f37
Dan <davis.dan.c@gmail.com>
2023-10-14 18:34:24 -0400

    update app.py

--- backend_build/app.py
+++ backend_build/app.py
@@ -2,7 +2,7 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-config = {
+user_config = {
     'user': 'user',
     'password': 'pass',
     'host': 'db',
@@ -10,9 +10,16 @@ config = {
     'raise_on_warnings': True
 }
 
+root_config = {
+    'user': 'root',
+    'password': 'pass',
+    'host': 'db',
+    'database': 'db',
+    'raise_on_warnings': True
+}
 
 try:
-  cnx = mysql.connector.connect(**config)
+  user_cnx = mysql.connector.connect(**user_config)
 except mysql.connector.Error as err:
   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
     print("Something is wrong with your user name or password")
@@ -21,7 +28,22 @@ except mysql.connector.Error as err:
   else:
     print(err)
 else:
-  cnx.close()
+  user_cnx.close()
 
-print('closing after successful open')
-cnx.close()
+print('closing after successful user connect')
+user_cnx.close()
+
+try:
+  root_cnx = mysql.connector.connect(**root_config)
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+  root_cnx.close()
+
+print('closing after successful root connect')
+root_cnx.close()
