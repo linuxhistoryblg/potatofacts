8472393657988441c2dbe09493af6519a7c59436
dan <linuxhistoryblg@gmail.com>
2023-10-15 11:01:12 -0400

    update app.py

--- backend_build/app.py
+++ backend_build/app.py
@@ -1,49 +1,62 @@
-# Set up db connection
+# app.py - Dan Davis
+# Queries db container and formats
+# output to forward to smsgateway
+
 import mysql.connector
 from mysql.connector import errorcode
+import time
 
-user_config = {
-    'user': 'user',
-    'password': 'pass',
-    'host': 'db',
-    'database': 'db',
-    'raise_on_warnings': True
-}
-
-root_config = {
-    'user': 'root',
-    'password': 'pass',
-    'host': 'db',
-    'database': 'db',
-    'raise_on_warnings': True
-}
-
-try:
-  user_cnx = mysql.connector.connect(**user_config)
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
+def make_user_cnx():
+  # Define connection properties
+  user_config = {
+      'user': 'user',
+      'password': 'pass',
+      'host': 'db',
+      'database': 'Potatofacts',
+      'raise_on_warnings': True
+  }
+  # Make db connection for user
+  try:
+    user_cnx = mysql.connector.connect(**user_config)
+    time.sleep(1)
+    print('closing after successful user connect')
+    user_cnx.close()
+  except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+      print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+      print("Database does not exist")
+    else:
+      print(err)
   else:
-    print(err)
-else:
-  user_cnx.close()
+    user_cnx.close()
 
-print('closing after successful user connect')
-user_cnx.close()
-
-try:
-  root_cnx = mysql.connector.connect(**root_config)
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
+def make_root_cnx():
+  # Define connection properties
+  root_config = {
+      'user': 'root',
+      'password': 'pass',
+      'host': 'db',
+      'database': 'Potatofacts',
+      'raise_on_warnings': True
+  }
+  # Make db connection for root
+  try:
+    root_cnx = mysql.connector.connect(**root_config)
+    time.sleep(1)
+    print('closing after successful root connect')
+    root_cnx.close()
+  except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+      print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+      print("Database does not exist")
+    else:
+      print(err)
   else:
-    print(err)
-else:
-  root_cnx.close()
+    root_cnx.close()
 
-print('closing after successful root connect')
-root_cnx.close()
+
+# Test connect functions:
+make_user_cnx()
+make_root_cnx()
